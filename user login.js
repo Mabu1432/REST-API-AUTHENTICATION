5f81e56c8b9e67c8e5745783635dfb60528c7d9c
Mabu1432 <91015400+Mabu1432@users.noreply.github.com>
2022-01-09 19:55:35 +0530

    Create user login.js

---
+++ user login.js
@@ -0,0 +1,27 @@
+app.post('/users/signin', function (req, res) {
+  const user = req.body.username;
+  const pwd = req.body.password;
+ 
+  // return 400 status if username/password is not exist
+  if (!user || !pwd) {
+    return res.status(400).json({
+      error: true,
+      message: "Username or Password is required."
+    });
+  }
+ 
+  // return 401 status if the credential is not match.
+  if (user !== userData.username || pwd !== userData.password) {
+    return res.status(401).json({
+      error: true,
+      message: "Username or Password is wrong."
+    });
+  }
+ 
+  // generate token
+  const token = utils.generateToken(userData);
+  // get basic user details
+  const userObj = utils.getCleanUser(userData);
+  // return the token along with user details
+  return res.json({ user: userObj, token });
+});
